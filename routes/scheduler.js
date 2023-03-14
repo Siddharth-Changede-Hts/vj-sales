4b094fbb67cfee64c5539dc20a4c854f70540cfe
Siddharth Changede <siddharth.changede@htstechsolutions.com>
2023-03-14 19:01:51 +0530

    change

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -9,87 +9,87 @@ const agent = new https.Agent({
     rejectUnauthorized: false
 });
 
-// const job1 = schedule.scheduleJob('00 6 * * *', function () {
-//     // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
-//     //     console.log(allotmentPaymentRes)
-//     // })
-//     console.log('00 6');
-// });
-// const job2 = schedule.scheduleJob('0 6 * * *', function () {
-//     // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
-//     //     console.log(allotmentPaymentRes)
-//     // })
-//     console.log('0 6');
-// });
-// const job3 = schedule.scheduleJob('00 06 * * *', function () {
-//     // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
-//     //     console.log(allotmentPaymentRes)
-//     // })
-//     console.log('00 06');
-// });
-// const job4 = schedule.scheduleJob('0 06 * * *', function () {
-//     // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
-//     //     console.log(allotmentPaymentRes)
-//     // })
-//     console.log('0 06');
-// });
-// const job5 = schedule.scheduleJob('59 5 * * *', function () {
-//     // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
-//     //     console.log(allotmentPaymentRes)
-//     // })
-//     console.log('59 5');
-// });
-// const job6 = schedule.scheduleJob('59 05 * * *', function () {
-//     // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
-//     //     console.log(allotmentPaymentRes)
-//     // })
-//     console.log('59 05');
-// });
+const job1 = schedule.scheduleJob('00 6 * * *', function () {
+    // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
+    //     console.log(allotmentPaymentRes)
+    // })
+    console.log('00 6');
+});
+const job2 = schedule.scheduleJob('0 6 * * *', function () {
+    // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
+    //     console.log(allotmentPaymentRes)
+    // })
+    console.log('0 6');
+});
+const job3 = schedule.scheduleJob('00 06 * * *', function () {
+    // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
+    //     console.log(allotmentPaymentRes)
+    // })
+    console.log('00 06');
+});
+const job4 = schedule.scheduleJob('0 06 * * *', function () {
+    // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
+    //     console.log(allotmentPaymentRes)
+    // })
+    console.log('0 06');
+});
+const job5 = schedule.scheduleJob('59 5 * * *', function () {
+    // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
+    //     console.log(allotmentPaymentRes)
+    // })
+    console.log('59 5');
+});
+const job6 = schedule.scheduleJob('59 05 * * *', function () {
+    // supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
+    //     console.log(allotmentPaymentRes)
+    // })
+    console.log('59 05');
+});
 
-// router.get('/check', function (req, res, next) {
-//     supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "Payment Complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
-//         if (allotmentPaymentRes.data.length !== 0) {
-//             let vjUnits = []
-//             let landOwnerUnits = []
-//             allotmentPaymentRes.data.forEach((ele) => {
-//                 if (ele.unitId && ele.unitId.unitId) {
-//                     let booking_id = ''//CALL PRANIL API TO GET BOOKING ID
-//                     if (booking_id !== '') {
-//                         supabase.from('AllotmentPayment').update({ booking_id: booking_id }).eq('allotmentPaymentId', ele.allotmentPaymentId).then((updateRes) => {
-//                             console.log("success")
-//                         })
-//                     } else {
-//                         if (ele.unitId.farvisionStatus === 'Blocked') {
-//                             if (ele.unitId.isSanctioned) {
-//                                 landOwnerUnits.push(ele)
-//                             }
-//                         } else {
-//                             vjUnits.push(ele)
-//                         }
-//                     }
-//                 }
-//             })
-//         }
-//     })
-// })
+router.get('/check', function (req, res, next) {
+    supabase.from('AllotmentPayment').select('*,unitId(*),inventoryMergedId(*)').eq('status', "Payment Complete").eq('kycFormFilled', true).then((allotmentPaymentRes) => {
+        if (allotmentPaymentRes.data.length !== 0) {
+            let vjUnits = []
+            let landOwnerUnits = []
+            allotmentPaymentRes.data.forEach((ele) => {
+                if (ele.unitId && ele.unitId.unitId) {
+                    let booking_id = ''//CALL PRANIL API TO GET BOOKING ID
+                    if (booking_id !== '') {
+                        supabase.from('AllotmentPayment').update({ booking_id: booking_id }).eq('allotmentPaymentId', ele.allotmentPaymentId).then((updateRes) => {
+                            console.log("success")
+                        })
+                    } else {
+                        if (ele.unitId.farvisionStatus === 'Blocked') {
+                            if (ele.unitId.isSanctioned) {
+                                landOwnerUnits.push(ele)
+                            }
+                        } else {
+                            vjUnits.push(ele)
+                        }
+                    }
+                }
+            })
+        }
+    })
+})
 
-// const statusUpdates = schedule.scheduleJob('00 6 * * *', function () {
-//     supabase.from('Projects').select('*').then(async (projectRes) => {
-//         let count = 0
-//         for (const project of projectRes.data) {
-//             let matchRes = await matchUnits(project)
-//             console.log(count++, new Date().toDateString())
-//         }
-//         console.log("end")
-//         // res.send("done")
-//         // projectRes.data.forEach(async (project, index) => {
-//         //     if (index === projectRes.data.length - 1) {
-//         //     } else {
-//         //         console.log(index)
-//         //     }
-//         // })
-//     })
-// })
+const statusUpdates = schedule.scheduleJob('00 6 * * *', function () {
+    supabase.from('Projects').select('*').then(async (projectRes) => {
+        let count = 0
+        for (const project of projectRes.data) {
+            let matchRes = await matchUnits(project)
+            console.log(count++, new Date().toDateString())
+        }
+        console.log("end")
+        // res.send("done")
+        // projectRes.data.forEach(async (project, index) => {
+        //     if (index === projectRes.data.length - 1) {
+        //     } else {
+        //         console.log(index)
+        //     }
+        // })
+    })
+})
 
 function matchUnits(project) {
     return new Promise((resolve, reject) => {
